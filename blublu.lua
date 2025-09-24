abbfb746161ea020204a82e7360c61601e24abf4
Airplane <213837125+TheAirplaneMan@users.noreply.github.com>
2025-09-23 23:35:11 -0800

    Update blublu.lua

--- blublu.lua
+++ blublu.lua
@@ -13,8 +13,11 @@ minetest.register_node("mccontent:blublu_block", {
 minetest.register_node("mccontent:blublu_pillar", {
 	description = ("Blublu Pillar"),
 	tiles = {"mccontent_blublu_pillar_top.png", "mccontent_blublu_pillar_top.png", "mccontent_blublu_pillar.png"},
+	paramtype2 = "facedir",
 	groups = {cracky = 3, blublu = 1},
 	sounds = default.node_sound_stone_defaults(),
+
+	on_place = minetest.rotate_node
 })
 
 minetest.register_craft({
@@ -40,3 +43,16 @@ minetest.register_craft({
 		{"mccontent:blublu_block"},
 	}
 })
+
+
+
+if core.get_modpath("stairs") then
+
+	stairs.register_stair_and_slab("blublu_block", "mccontent:blublu_block",
+		{cracky = 3},
+		{"mccontent_blublu_block.png"},
+		("Blublu Block Stair"),
+		("Blublu Block Slab"),
+		default.node_sound_stone_defaults())
+
+end

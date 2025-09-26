d968e83d381f53789ecb4af4ba1a3b78b6905290
Airplane <213837125+TheAirplaneMan@users.noreply.github.com>
2025-09-25 21:31:15 -0800

    Update pyramids.lua

--- pyramids.lua
+++ pyramids.lua
@@ -8,7 +8,7 @@
 		y_max = 31000,
 		y_min = 1,
 		schematic = minetest.get_modpath("mccontent") .. "/schematics/sandstone_desert_pyramid.mts",
-		flags = "place_center_x, place_center_z",
+		flags = "place_center_x, place_center_z, force_placement",
 		place_offset_y = -6,
 		rotation = "random",
 	})

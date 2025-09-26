bb6dcec31330e422be6885251c149f735b3263a8
Airplane <213837125+TheAirplaneMan@users.noreply.github.com>
2025-09-25 21:46:48 -0800

    Add a new structure for silver desserts, ermmm, deserts.

--- pyramids.lua
+++ pyramids.lua
@@ -12,3 +12,17 @@
 		place_offset_y = -5,
 		rotation = "random",
 	})
+
+	minetest.register_decoration({
+		name = "mccontent:cold_desert_spirecake",
+		deco_type = "schematic",
+		place_on = {"default:silver_sand"},
+		sidelen = 80,
+		fill_ratio = 0.000001,
+		biomes = {"cold_desert"},
+		y_max = 31000,
+		y_min = 1,
+		schematic = minetest.get_modpath("mccontent") .. "/schematics/cold_desert_spirecake.mts",
+		flags = "place_center_x, place_center_z, force_placement",
+		rotation = "random",
+	})
